73a4facbd11ec853035be34fccfa3eb926555958
YeonwooSung <neos960518@gmail.com>
2020-02-27 06:04:13 +0900

    Get lists of all possible combinations of verbs and nouns, verbs and objects, and nouns and objects

--- mergeResults.py
+++ mergeResults.py
@@ -1,6 +1,6 @@
 from __future__ import division
 import argparse
-from itertools import product, permutations
+from itertools import product
 import nltk
 from nltk.corpus import wordnet as wn
 
@@ -47,7 +47,7 @@ def calculateSimilarity(word1, word2):
         max_sims = max(sims)
         checker = True
     else:
-        print('calculateSimilarity::Error ==> word1=({}), word2=({})'.format(word1, word2))
+        print('[DEBUG] calculateSimilarity::Error ==> word1=({}), word2=({})'.format(word1, word2))
     return max_sims, checker
 
 
@@ -85,9 +85,9 @@ def readActionDetectionResult(f, exec_mode):
 
     # check if the user select "debug" mode
     if exec_mode == 'debug':
-        print(info_line)
-        print(v_line)
-        print(n_line)
+        print('[DEBUG] ' + info_line)
+        print('[DEBUG] ' + v_line)
+        print('[DEBUG] ' + n_line)
 
     v = v_line.replace('v=', '')
     n_num = int(n_line.replace('n=', ''))
@@ -108,27 +108,65 @@ def readObjectDetectionResult(f, exec_mode):
                 objList.append(obj)
             frame_num = line.replace('current frame:', '').strip()
             obj = {}
-            obj['frame_num'] = frame_num
+            obj['frame_num'] = int(frame_num)
             obj['objects'] = []
         else:
             obj['objects'].append(line)
 
             # check if the user select "debug" mode
             if exec_mode == 'debug':
-                print(line)
+                print('[DEBUG] ' + line)
     if obj:
         objList.append(obj)
     # check if the user select "debug" mode
     if exec_mode == 'debug':
-        print('Length of objList= {}'.format(len(objList)))
+        print('[DEBUG] Length of objList= {}'.format(len(objList)))
 
     return objList
+
+
+def getAllCombinationsOf2Lists(list1, list2):
+    all_combinations = [[i, j] for i in list1 for j in list2]
+    return all_combinations
+
+
+def checkFrameRangeForSimilarityCalculation(start_frame, end_frame, objList, v, n_list, index, exec_mode):
+    while True:
+        # to avoid IndexOutOfBounds error
+        if index >= len(objList):
+            break
+        # get a object from the object list
+        obj = objList[index]
+        frame_num = obj['frame_num']
+
+        # print out debug message if it is debugging mode
+        if exec_mode == 'debug':
+            print('[DEBUG] frame={}, start_frame={}, end_frame={}'.format(frame_num, start_frame, end_frame))
+
+        # check the range of frame number
+        if start_frame <= frame_num and end_frame >= frame_num:
+            objects = obj['objects']
+            v_list = [v]
+
+            # get a list of all possible combinations of verbs and nouns
+            combinations_v_n = getAllCombinationsOf2Lists(v_list, n_list)
+            # get a list of all possible combinations of verbs and objects
+            combinations_v_obj = getAllCombinationsOf2Lists(v_list, objects)
+            # get a list of all possible combinations of nouns and objects
+            combinations_n_obj = getAllCombinationsOf2Lists(n_list, objects)
+
+            #TODO
+
+            index += 1
+        else:
+            break
+    return index
 
 
 if __name__ == '__main__':
     args = arg_parse()  # generate argument parser
 
-    exec_mode = args.mode
+    exec_mode = args.mode  # get the execution mode (either normal or debug)
 
     # file path of result files
     obj_output_file_path = args.obj_result
@@ -139,6 +177,12 @@ if __name__ == '__main__':
 
     # read object lists via file stream
     objList = readObjectDetectionResult(f_obj, exec_mode)
+    index = 0
+
+    #TODO store the previous verbs and nouns -> compare with current verb and nouns
+    prev_v = ''
+    prev_n = []
+    start = 0
 
     while True:
         info_line, v, n = readActionDetectionResult(f_act, exec_mode)
@@ -151,8 +195,15 @@ if __name__ == '__main__':
 
         # get start and end frame number from the info_line
         frame_nums = info_line.replace(':', '').replace('from', '').strip().split(' to ')
-        start_frame = frame_nums[0].strip()
-        end_frame = frame_nums[1].strip()
+        start_frame = int(frame_nums[0].strip())
+        end_frame = int(frame_nums[1].strip())
+
+        if prev_v != v and start != start_frame:
+            #TODO change point
+            if exec_mode == 'debug':
+                print('[DEBUG] change point detected - prev_v={}, v={}'.format(prev_v, v))
+
+        index = checkFrameRangeForSimilarityCalculation(start_frame, end_frame, objList, v, n, index, exec_mode)
 
         #TODO 1) compare verb and nouns -> remove unnecessary nouns
         #TODO 2) compare verbs and objects -> remove unnecessary objects
