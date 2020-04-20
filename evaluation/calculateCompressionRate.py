c3a8b1780a8efadf0a19087cbfaa09f1f7fab0f2
YeonwooSung <neos960518@gmail.com>
2020-04-20 15:04:11 +0900

    Update TODO list

--- evaluation/calculateCompressionRate.py
+++ evaluation/calculateCompressionRate.py
@@ -36,5 +36,3 @@ if __name__ == '__main__':
     epic_train_min, epic_train_max, epic_train_avg = iterate_df(epic_train_df)
     print('EpicKitchens - Testing')
     epic_test_min, epic_test_max, epic_test_avg = iterate_df(epic_test_df)
-
-    #TODO
